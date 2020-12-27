04a6d536db17e8ae0150b7669f21345c24432bf0
emilio4585 <emiliosalas54@gmail.com>
2020-12-26 23:03:45 -0600

    added blog project

--- mb_project/posts/tests.py
+++ mb_project/posts/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
-
+from .models import Post
 # Create your tests here.
+
+class PostModelTest(TestCase):
+    def setUp(self):
+        Post.objects.create(text="Solo un texto")
+        Post2.objects.create(text="Solo un texto 2")
+
+    def test_text_content_1(self):
+        post = Post.objects.get(id=1)
+        expected_object_name = f"{post.text}"
+        self.assertEqual(expected_object_name, "Solo un texto")
+
