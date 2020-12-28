27faf7b110673611201b11baa70b246a49e0b4ed
emilio4585 <emiliosalas54@gmail.com>
2020-12-27 22:57:29 -0600

    Added forms

--- blog_project/blog/urls.py
+++ blog_project/blog/urls.py
@@ -2,10 +2,17 @@ from django.urls import path
 from .views import (
     BlogListView,
     BlogDetailView,
+    BlogCreateView,
+    BlogEditView,
+    BlogDeleteView,
     )
 
 urlpatterns = [
     path('', BlogListView.as_view(), name='home'),
-    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
+    path('post/new/', BlogCreateView.as_view(), name='post_new'),               #Create
+    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),       #Read
+    path('post/<int:pk>/edit/', BlogEditView.as_view(), name='post_edit'),      #Update
+    path('post/<int:pk>/delete/', BlogDeleteView.as_view(), name='post_delete'),#Delete
+    
 ]
 #http://127.0.0.1:8000/post/2/
