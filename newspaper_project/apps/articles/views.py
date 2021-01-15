9ad721612a261a7453812b0c507f04458d6d74c2
emilio4585 <emiliosalas54@gmail.com>
2021-01-15 01:14:39 -0600

    Añadi modificaciones

--- newspaper_project/apps/articles/views.py
+++ newspaper_project/apps/articles/views.py
@@ -1,31 +1,41 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy
 from .models import Article
 # Create your views here.
 
-class ArticleListView(ListView):
+class ArticleListView(LoginRequiredMixin, ListView):
     model = Article
     context_object_name = 'articles'
     template_name = 'articles/article_list.html'
+    login_url = 'login'
 
-class ArticleDetailView(DetailView):
+class ArticleDetailView(LoginRequiredMixin,DetailView):
     model = Article
     context_object_name = 'article'
     template_name = 'articles/article_detail.html'
+    login_url = 'login'
 
-class ArticleUpdateView(UpdateView):
+class ArticleUpdateView(LoginRequiredMixin, UpdateView):
     model = Article
     fields = ('title','body',)
     template_name = 'articles/article_edit.html'
+    login_url = 'login'
 
-class ArticleDeleteView(DeleteView):
+class ArticleDeleteView(LoginRequiredMixin, DeleteView):
     model = Article
     template_name = 'articles/article_delete.html'
     success_url = reverse_lazy('article_list')
-
-class ArticleCreateView(CreateView):
+    login_url = 'login'
+    
+class ArticleCreateView(LoginRequiredMixin,CreateView):
     model = Article
     template_name = 'articles/article_new.html'
-    fields = ('title', 'body', 'author')
+    fields = ('title', 'body')
+    login_url = 'login'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 #http://127.0.0.1:8000/articles
